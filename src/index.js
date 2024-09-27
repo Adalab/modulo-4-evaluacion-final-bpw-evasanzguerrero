cac4a9216c887fc41b92baa4beaa177633d835d2
evasanzgue <evasanzgue@gmail.com>
2024-09-27 20:31:55 +0200

    Add endpoints about zones, get list and by id

--- src/index.js
+++ src/index.js
@@ -7,37 +7,150 @@ server.get('/series',async (_, res) => {
   const conn = await getConnection();
   
   if(!conn) {
-    res.status(500).send('Se rompió');
+    res.status(500).send('connection lost');
     return;
   }
 
-  const [results] = await conn.query('Select * from serie;');
+  const [results] = await conn.query('Select * from series;');
 
-  res.send(results);
+  const data = {
+    success: true,
+    series: results
+  };
+  res.json(data);
+
+  conn.close();
 });
 
 server.get('/series/:id',async (req, res) => {
   const conn = await getConnection();
 
   if(!conn) {
-    res.status(500).send('Se rompió');
+    res.status(500).send('connection lost');
     return;
   }
 
-  const [results] = await conn.query('Select * from serie where id=?;',[req.params.id]);
+  const [results] = await conn.query('Select * from series where id=?;',[req.params.id]);
 
-  res.send(results);
+  const data = {
+    success: true,
+    serie: results
+  };
+  res.json(data);
+
+  conn.close();
 });
 
 server.get('/series/titulo/:titulo',async (req, res) => {
   const conn = await getConnection();
   
   if(!conn) {
-    res.status(500).send('Se rompió');
+    res.status(500).send('connection lost');
     return;
   }
   
-  const [results] = await conn.query('Select * from serie where titulo like ?;',[`%${req.params.titulo}%`]);
+  const [results] = await conn.query('Select * from series where titulo like ?;',[`%${req.params.titulo}%`]);
 
-  res.send(results);
+  const data = {
+    success: true,
+    series: results
+  };
+  res.json(data);
+
+  conn.close();
+});
+
+server.get('/personajes',async (_, res) => {
+  const conn = await getConnection();
+  
+  if(!conn) {
+    res.status(500).send('connection lost');
+    return;
+  }
+
+  const [results] = await conn.query('Select * from personajes;');
+
+  const data = {
+    success: true,
+    personajes: results
+  };
+  res.json(data);
+
+  conn.close();
+});
+
+server.get('/personajes/:id',async (req, res) => {
+  const conn = await getConnection();
+
+  if(!conn) {
+    res.status(500).send('connection lost');
+    return;
+  }
+  
+  const [results] = await conn.query('Select * from personajes where id=?;',[req.params.id]);
+
+  const data = {
+    success: true,
+    personaje: results
+  };
+  res.json(data);
+
+  conn.close();
+});
+
+server.get('/personajes/nombre/:nombre',async (req, res) => {
+  const conn = await getConnection();
+
+  if(!conn) {
+    res.status(500).send('connection lost');
+    return;
+  }
+  
+  const [results] = await conn.query('Select * from personajes where nombre like ?;',[`%${req.params.nombre}%`]);
+
+  const data = {
+    success: true,
+    nombre: results
+  };
+  res.json(data);
+
+  conn.close();
+});
+
+server.get('/lugares',async (req, res) => {
+  const conn = await getConnection();
+  
+  if(!conn) {
+    res.status(500).send('connection lost');
+    return;
+  }
+
+  const [results] = await conn.query('Select * from lugares;');
+
+  const data = {
+    success: true,
+    lugar: results
+  };
+  res.json(data);
+
+  conn.close();
+});
+
+server.get('/lugares/:id',async (req, res) => {
+  const conn = await getConnection();
+
+  if(!conn) {
+    res.status(500).send('connection lost');
+    return;
+  }
+  
+  const [results] = await conn.query('Select * from lugares where id=?;',[req.params.id]);
+
+  const data = {
+    success: true,
+    lugares: results
+  };
+  res.json(data);
+
+  conn.close();
 });
